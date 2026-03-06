a4a641ab557210b21c5fe554576a671c338e50cb
creamgod45 <fuyin1054@gmail.com>
2026-03-06 20:51:47 +0800

    Initial Commit

---
+++ TeamChatUPTests/TeamChatUPTests.swift
@@ -0,0 +1,16 @@
+//
+//  TeamChatUPTests.swift
+//  TeamChatUPTests
+//
+//  Created by creamgod45 on 2026/3/6.
+//
+
+import Testing
+
+struct TeamChatUPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
